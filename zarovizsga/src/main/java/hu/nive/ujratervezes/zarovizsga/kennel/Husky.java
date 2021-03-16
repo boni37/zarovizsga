d8484d226de6ba0fc107d2c721ac614eb06fe4ee
boni37 <73605283+boni37@users.noreply.github.com>
2021-03-16 20:31:08 +0100

    zarovizsga letoltes

--- zarovizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/kennel/Husky.java
+++ zarovizsga/src/main/java/hu/nive/ujratervezes/zarovizsga/kennel/Husky.java
@@ -1,4 +1,25 @@
 package hu.nive.ujratervezes.zarovizsga.kennel;
 
-public class Husky {
+public class Husky extends Dog{
+
+    private int happiness;
+
+    public Husky(String name) {
+        super(name);
+    }
+
+    @Override
+    public int getHappiness() {
+        return happiness;
+    }
+
+    @Override
+    public void play(int i) {
+        this.happiness += i*3;
+    }
+
+    @Override
+    public void feed() {
+        this.happiness += 4;
+    }
 }
